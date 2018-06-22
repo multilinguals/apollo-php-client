a7df6f5bf18ad990fd447001e65bb8773f93fa25
zangkun <04041143@163.com>
2018-06-22 09:04:43 +0800

    格式化代码

--- src/ApolloClient.php
+++ src/ApolloClient.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Org\Multilinguals\Apollo\Client;
 
 class ApolloClient
@@ -16,38 +17,42 @@ class ApolloClient
      * @param string $appId apollo配置项目的appid
      * @param array $namespaces apollo配置项目的namespace
      */
-    public function __construct($configServer, $appId, array $namespaces) {
+    public function __construct($configServer, $appId, array $namespaces)
+    {
         $this->configServer = $configServer;
         $this->appId = $appId;
         foreach ($namespaces as $namespace) {
-            $this->notifications[$namespace] = ['namespaceName'=>$namespace,'notificationId'=>-1];
+            $this->notifications[$namespace] = ['namespaceName' => $namespace, 'notificationId' => -1];
         }
         $this->save_dir = dirname($_SERVER['SCRIPT_FILENAME']);
     }
 
-    public function setCluster($cluster) {
+    public function setCluster($cluster)
+    {
         $this->cluster = $cluster;
     }
 
-    public function setClientIp($ip) {
+    public function setClientIp($ip)
+    {
         $this->clientIp = $ip;
     }
 
     //获取配置-无缓存的方式
-    public function pull_config($namespaceName) {
-        $base_api = rtrim($this->configServer, '/').'/configs/'.$this->appId.'/'.$this->cluster.'/';
-        $api = $base_api.$namespaceName;
+    public function pull_config($namespaceName)
+    {
+        $base_api = rtrim($this->configServer, '/') . '/configs/' . $this->appId . '/' . $this->cluster . '/';
+        $api = $base_api . $namespaceName;
         $args = [];
         $args['releaseKey'] = '';
         $args['ip'] = $this->clientIp;
 
-        $config_file = $this->save_dir.DIRECTORY_SEPARATOR.'apolloConfig.'.$namespaceName.'.php';
+        $config_file = $this->save_dir . DIRECTORY_SEPARATOR . 'apolloConfig.' . $namespaceName . '.php';
         if (file_exists($config_file)) {
             $last_config = require $config_file;
             is_array($last_config) && isset($last_config['releaseKey']) && $args['releaseKey'] = $last_config['releaseKey'];
         }
 
-        $api .= '?'.http_build_query($args);
+        $api .= '?' . http_build_query($args);
 
         $ch = curl_init($api);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
@@ -55,7 +60,7 @@ class ApolloClient
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         $body = curl_exec($ch);
-        $httpCode = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $error = curl_error($ch);
         curl_close($ch);
         if ($body === false) {
@@ -68,7 +73,7 @@ class ApolloClient
 
         if ($httpCode == 200) {
             $result = json_decode($body, true);
-            $content = '<?php return '.var_export($result, true).';';
+            $content = '<?php return ' . var_export($result, true) . ';';
             file_put_contents($config_file, $content);
         }
     }
@@ -79,7 +84,7 @@ class ApolloClient
      */
     public function start($callback = null)
     {
-        $base_url = rtrim($this->configServer, '/').'/notifications/v2?';
+        $base_url = rtrim($this->configServer, '/') . '/notifications/v2?';
         $params = [];
         $params['appId'] = $this->appId;
         $params['cluster'] = $this->cluster;
@@ -93,9 +98,9 @@ class ApolloClient
             do {
                 $params['notifications'] = json_encode(array_values($this->notifications));
                 $query = http_build_query($params);
-                curl_setopt($ch, CURLOPT_URL, $base_url.$query);
+                curl_setopt($ch, CURLOPT_URL, $base_url . $query);
                 $response = curl_exec($ch);
-                $httpCode = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
                 $error = curl_error($ch);
                 if ($response === false) {
                     throw new \Exception($error);
@@ -114,17 +119,17 @@ class ApolloClient
                                 $this->pull_config($r['namespaceName']);
                                 //配置变更-更新notificationId
                                 $this->notifications[$r['namespaceName']]['notificationId'] = $r['notificationId'];
-                            }catch (\Exception $e) {
+                            } catch (\Exception $e) {
                                 //某个namespace拉取配置失败的时候，不影响其它namespace
-                                echo $e->getMessage()."\n";
+                                echo $e->getMessage() . "\n";
                             }
                         }
                     }
                     //如果定义了配置变更的回调，比如重新整合配置，则执行回调
                     ($callback instanceof \Closure) && call_user_func($callback);
                 }
-            }while (1);
-        }catch (\Exception $e) {
+            } while (1);
+        } catch (\Exception $e) {
             curl_close($ch);
             return $e->getMessage();
         }
